be9fbb16248cd0f8c2d5d439dc1d93eba7d2db1d
LuizFernandoFS <lffs2000@outlook.com>
2023-10-04 21:40:06 -0300

    FEAT: PerguntaFactory e desserialização json.

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
+import jsonpickle
 
 # Imports locais do meu projeto
-from model.JsonTools import LoadData
 from model.Pergunta import Pergunta
 
 app = Flask(__name__)
 
 @app.route("/perguntas")
 def perguntas():
-    loader = LoadData()
-    dados = loader.getJson('./assets/perguntas.json')
-    perguntas = Pergunta.carregar_perguntas(dados)
-    return render_template('perguntas.html', perguntas=perguntas)
+    perguntas_dicionario = Pergunta.carregar_dicionario_de_perguntas()
+    return Pergunta.serialize_json(perguntas_dicionario)
+
+@app.route("/perguntas/<tema>")
+def perguntas_tema_especifico(tema):
+    perguntas_dicionario = Pergunta.carregar_dicionario_de_perguntas()
+    perguntas_tema = Pergunta.filtrar_perguntas_por_tema(perguntas_dicionario, tema)   
+    return Pergunta.serialize_json(perguntas_tema)
 
 if __name__ == '__main__':
     app.debug = True
--- model/Pergunta.py
+++ model/Pergunta.py
@@ -1,5 +1,24 @@
+from flask import Response
+import jsonpickle
+
+# Meus imports
+from model.JsonTools import LoadData
+
+class PerguntaFactory:
+    @staticmethod
+    def criar_pergunta(pergunta_json):
+        return Pergunta(
+            pergunta_json["id"],
+            pergunta_json["pergunta"],
+            pergunta_json["opcoes"],
+            pergunta_json["resposta_correta"],
+            pergunta_json["tema"],
+            pergunta_json["nivel_dificuldade"]
+        )
+
 class Pergunta:
-    def __init__(self, pergunta, opcoes, resposta_correta, tema, nivel_dificuldade):
+    def __init__(self, id, pergunta, opcoes, resposta_correta, tema, nivel_dificuldade):
+        self.id = id
         self.pergunta = pergunta
         self.opcoes = opcoes
         self.resposta_correta = resposta_correta
@@ -8,17 +27,28 @@ class Pergunta:
 
     @staticmethod
     def carregar_perguntas(dados_do_json):
-        # Cria um ARRAY de perguntas
         perguntas = []
         for pergunta_json in dados_do_json["perguntas"]:
-            # Popula o objeto com base no JSON
-            pergunta = Pergunta(
-                pergunta_json["pergunta"],
-                pergunta_json["opcoes"],
-                pergunta_json["resposta_correta"],
-                pergunta_json["tema"],
-                pergunta_json["nivel_dificuldade"]
-            )
-            # Adiciona PERGUNTA ao ARRAY de perguntas
+            pergunta = PerguntaFactory.criar_pergunta(pergunta_json)
             perguntas.append(pergunta)
         return perguntas
+    
+    @staticmethod
+    def filtrar_perguntas_por_tema(perguntas, tema):
+        perguntas_tema = []
+        for pergunta in perguntas:
+            if pergunta.tema == tema.upper():
+                perguntas_tema.append(pergunta)     
+        return perguntas_tema
+    
+    @staticmethod
+    def carregar_dicionario_de_perguntas():
+        loader = LoadData()
+        dados = loader.getJson('./assets/perguntas.json')
+        perguntas = Pergunta.carregar_perguntas(dados)
+        return perguntas
+    
+    @staticmethod
+    def serialize_json(perguntas):
+        response = jsonpickle.encode(perguntas, unpicklable=False)    
+        return Response(response, mimetype="application/json")
