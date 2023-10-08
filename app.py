efcebc3283434031675db0c2141a920cad56de4d
LuizFernandoFS <lffs2000@outlook.com>
2023-10-08 00:08:04 -0300

    Criado a estratégia para gerar as perguntas

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, jsonify
-import jsonpickle
+from flask import Flask
 
 # Imports locais do meu projeto
-from model.Pergunta import Pergunta
+from model.Pergunta import Pergunta, EstrategiaPerguntas
 
 app = Flask(__name__)
 
@@ -16,6 +15,20 @@ def perguntas_tema_especifico(tema):
     perguntas_dicionario = Pergunta.carregar_dicionario_de_perguntas()
     perguntas_tema = Pergunta.filtrar_perguntas_por_tema(perguntas_dicionario, tema)   
     return Pergunta.serialize_json(perguntas_tema)
+
+@app.route("/gerar/perguntas/<tema>/<int:quantidade>")
+def gerar_perguntas_tema_quantidade(tema, quantidade):
+    perguntas = Pergunta.carregar_dicionario_de_perguntas()
+    estrategia = EstrategiaPerguntas(perguntas)
+    perguntas_por_tema = estrategia.gerar_perguntas_por_tema(quantidade, tema)
+    return Pergunta.serialize_json(perguntas_por_tema)
+
+@app.route("/gerar/perguntas/aleatorias/<int:quantidade>")
+def gerar_perguntas_aleatorias(quantidade):
+    perguntas = Pergunta.carregar_dicionario_de_perguntas()
+    estrategia = EstrategiaPerguntas(perguntas)
+    perguntas_aleatorias = estrategia.gerar_perguntas_aleatorias(quantidade)
+    return Pergunta.serialize_json(perguntas_aleatorias)
 
 if __name__ == '__main__':
     app.debug = True
--- model/Pergunta.py
+++ model/Pergunta.py
@@ -1,9 +1,11 @@
+import random
 from flask import Response
 import jsonpickle
 
 # Meus imports
 from model.JsonTools import LoadData
 
+# FACTORY para criar perguntas
 class PerguntaFactory:
     @staticmethod
     def criar_pergunta(pergunta_json):
@@ -52,3 +54,25 @@ class Pergunta:
     def serialize_json(perguntas):
         response = jsonpickle.encode(perguntas, unpicklable=False)    
         return Response(response, mimetype="application/json")
+
+# Utilizando STRATEGY para gerar perguntas randômicas ou com determinados temas
+class EstrategiaPerguntas:
+    def __init__(self, perguntas):
+        self.perguntas = perguntas
+
+    def selecionar_perguntas(self, num_perguntas, tema=None):
+        if tema is None:
+            return self.gerar_perguntas_aleatorias(num_perguntas)
+        else:
+            return self.gerar_perguntas_por_tema(num_perguntas, tema)
+
+    def gerar_perguntas_aleatorias(self, num_perguntas):
+        if num_perguntas >= len(self.perguntas):
+            return self.perguntas
+        return random.sample(self.perguntas, num_perguntas)
+
+    def gerar_perguntas_por_tema(self, num_perguntas, tema):
+        perguntas_do_tema = Pergunta.filtrar_perguntas_por_tema(self.perguntas, tema)
+        if num_perguntas >= len(perguntas_do_tema):
+            return perguntas_do_tema
+        return random.sample(perguntas_do_tema, num_perguntas)
